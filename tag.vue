f3f766ae3582aab7e2bef2a003ccebd5c0627dca
mahe <mahesh9415@gmail.com>
2017-09-12 15:53:17 +0530

    first commit

---
+++ tag.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+<div class="list_tags">
+	
+</div>
+
+</div>	
+</template>
+<script>
+import vue from 'Vue'
+	export default{
+			data(){
+				return{
+					tags:new Set(),
+					user_input:null
+				}
+			}
+	}
+</script>
+<style>
+	
+</style>
